c8e0f33e44028b1fca7a91dda0219e73f67cbf86
Nilesh Darade <ndarade@knurld.com>
2016-05-15 13:05:36 -0700

    minpr gix

--- src/main/java/com/knurld/rest/Twilio.java
+++ src/main/java/com/knurld/rest/Twilio.java
@@ -1,36 +1,50 @@
 package com.knurld.rest;
 
-import java.io.InputStream;
-import java.util.UUID;
-
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.knurld.dropbox.api.UploadAPI;
 import com.sun.jersey.multipart.FormDataParam;
+import com.twilio.sdk.verbs.Gather;
+import com.twilio.sdk.verbs.Play;
+import com.twilio.sdk.verbs.Say;
+import com.twilio.sdk.verbs.TwiMLException;
+import com.twilio.sdk.verbs.TwiMLResponse;
 
-@Path("/file")
+@Path("/twilio")
 public class Twilio {
+	// incomingCall
 
 	@POST
-	@Path("/upload")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
-			@FormDataParam("fileName") String fileName) {
-		if (fileName == null || fileName.isEmpty()) {
-			fileName = UUID.randomUUID().toString() + ".wav";
+	@Path("/incomingCall")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response incomingCall(@FormDataParam("Digits") String digits) {
+		// Create a TwiML response and add our friendly message.
+		TwiMLResponse twiml = new TwiMLResponse();
+		Say say = new Say("Hi There, you calling Knurld!");
 
-		}
-		String output = UploadAPI.uploadFile(uploadedInputStream, fileName);
-		if (output != null) {
-			return Response.status(200).entity(output).header("Access-Control-Allow-Origin", "*")
-					.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").build();
+		// Play an MP3 for incoming callers.
+		Play play = new Play("http://demo.twilio.com/hellomonkey/monkey.mp3");
 
-		} else {
-			return Response.status(500).entity("Not able to upload file").build();
+		Gather gather = new Gather();
+		gather.setAction("/handle-key");
+		gather.setNumDigits(1);
+		gather.setMethod("POST");
+		Say sayInGather = new Say("To speak to a real monkey, press 1. " + "Press 2 to record your own monkey howl. "
+				+ "Press any other key to start over.");
+		try {
+			gather.append(sayInGather);
+			twiml.append(say);
+			twiml.append(play);
+			twiml.append(gather);
+		} catch (TwiMLException e) {
+			e.printStackTrace();
 		}
+
+		return Response.status(200).entity(twiml).header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT").build();
+		
 	}
 }
